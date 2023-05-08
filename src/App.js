2873e0ba25f70d864005fb2a2a332dbb3a4f4431
David <ddaviddn109@gmail.com>
2023-05-08 01:22:47 -0500

    Finishing touches on social media icons, and header).

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import github_logo from './assets/noun-github-4289652.svg';
 
+/////////////////////////// FIRST PAGE (HOME) ////////////////////////////
 function StaticHeader() {
     return (<>
         <div className="App-header">
@@ -21,7 +23,24 @@ function StaticHeader() {
     </>)
 }
 
-function FirstPageComponent() {
+function SocialIcons() {
+    return (<div className="App-icons">
+        <p>
+            <a href="http://github.com/ddaviddn" target="_blank"><img className="Social-icons" src={github_logo} alt="github"/></a>
+            <a href="http://github.com/ddaviddn" target="_blank"><img className="Social-icons" src={github_logo} alt="github"/></a>
+            <a href="http://github.com/ddaviddn" target="_blank"><img className="Social-icons" src={github_logo} alt="github"/></a>
+            <a href="http://github.com/ddaviddn" target="_blank"><img className="Social-icons" src={github_logo} alt="github"/></a>
+        </p>
+        <p>
+            {/*<a href="https://github.com/ddaviddn" target="_blank"> o </a>*/}
+            {/*<code>*/}
+            {/*    o | o | o | o*/}
+            {/*</code>*/}
+        </p>
+    </div>)
+}
+
+function HomePage() {
     return (<>
         <div className="App-body">
             <p>
@@ -33,22 +52,30 @@ function FirstPageComponent() {
                         <code>data engineering | data science | data visualization </code>
                     </p>
                 </div>
-                <div className="App-icons">
-                    <p>
-                        {/*<a href="https://github.com/ddaviddn" target="_blank"> o </a>*/}
-                        <code> o | o | o | o </code>
-                    </p>
-                </div>
             </div>
+            <SocialIcons />
         </div>
     </>);
 }
 
+
+
+/////////////////////////// SECOND PAGE (PERSONAL) ////////////////////////////
+
+/////////////////////////// THIRD PAGE (CV) ////////////////////////////
+
+/////////////////////////// FOURTH PAGE (PORTFOLIO) ////////////////////////////
+
+
+
+
+
+/////////////////////////// FULL OUTPUT (ENTIRE SITE) ////////////////////////////
 function App() {
     return (<>
-        <StaticHeader/>
+        <StaticHeader />
         <div className="App">
-            <FirstPageComponent/>
+            <HomePage />
         </div>
     </>);
 }
