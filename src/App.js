dba2b225c77b6d18a45bb96fb2b75e1ffc0d9259
David <ddaviddn109@gmail.com>
2023-05-08 00:01:21 -0500

    Initial Header and main page description.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import './App.css';
 
+function StaticHeader() {
+    return (<>
+        <div className="App-header">
+            <></>
+            <p>
+                <a href="/">home</a>
+            </p>
+            <p>
+                <a href="/">personal</a>
+            </p>
+            <p>
+                <a href="/">cv</a>
+            </p>
+            <p>
+                <a href="/">portfolio</a>
+            </p>
+            <></>
+        </div>
+    </>)
+}
+
+function FirstPageComponent() {
+    return (<>
+        <div className="App-body">
+            <p>
+                <code>david nguyen</code>.
+            </p>
+            <div className="container">
+                <div className="App-typing">
+                    <p>
+                        <code>data engineering | data science | data visualization </code>
+                    </p>
+                </div>
+                <div className="App-icons">
+                    <p>
+                        {/*<a href="https://github.com/ddaviddn" target="_blank"> o </a>*/}
+                        <code> o | o | o | o </code>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </>);
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <code>david nguyen</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          // target="_blank"
-          // rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<>
+        <StaticHeader/>
+        <div className="App">
+            <FirstPageComponent/>
+        </div>
+    </>);
 }
 
 export default App;
---
+++ src/components/FirstPageComponent.js
@@ -0,0 +1,21 @@
+// import '../App.css';
+//
+// const FirstPageComponent = () => {
+//     return
+//     (<>
+//         <div className="App-header">
+//             <p>
+//                 <code>david nguyen</code>.
+//             </p>
+//             <div className="container">
+//                 <div className="App-typing">
+//                     <p>
+//                         <code>data engineer | data scientist | analytics engineer</code>
+//                     </p>
+//                 </div>
+//             </div>
+//         </div>
+//     </>);
+// };
+//
+// export default FirstPageComponent();
