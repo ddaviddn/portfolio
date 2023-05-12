7aa765e266c0400eac159be85291df2f6ebe90b3
David <ddaviddn109@gmail.com>
2023-05-12 01:06:57 -0500

    Tweaks to personal page, and starting on CV page.

--- src/components/CVPage/CVPage.js
+++ src/components/CVPage/CVPage.js
@@ -18,7 +18,7 @@ function CVPage() {
                             </code>
                         </p>
                     </div>
-                    <hr style={{borderWidth: "1.8px", width: "98%"}}/>
+                    <hr style={{borderWidth: "1.8px", width: "98%", marginTop: "-7.5px"}}/>
                     <div className="App-cv-subject-date">
                         <p className="Subject">
                             <p><code>Education</code></p>
@@ -36,7 +36,11 @@ function CVPage() {
                     </div>
                     <div className="App-cv-subject-date" style={{marginTop: "-70px"}}>
                         <p className="Subject">
-                            <p><code>Master's of Science in Analytics</code></p>
+                            <p>
+                                {/*<code>*/}
+                                    Master's of Science in Analytics
+                                {/*</code>*/}
+                            </p>
                         </p>
                         <p className="Subject">
                             <p><code>Overall GPA: 4.0 / 4.0</code></p>
@@ -53,7 +57,11 @@ function CVPage() {
                     </div>
                     <div className="App-cv-subject-date" style={{marginTop: "-70px"}}>
                         <p className="Subject">
-                            <p><code>Bachelor's of Science in Statistics and minor in Mathematics</code></p>
+                            <p>
+                                {/*<code>*/}
+                                    Bachelor's of Science in Statistics and minor in Mathematics
+                                {/*</code>*/}
+                            </p>
                         </p>
                         <p className="Subject">
                             <p><code>Overall GPA: 3.9 / 4.0</code></p>
@@ -68,19 +76,14 @@ function CVPage() {
                     <div className="App-cv-content">
                         <ul style={{marginTop: "-10px"}}>
                             <li style={{textAlign: "start"}}>
-                                <code>
                                     Python (pandas, scikit-learn, PyTorch, Streamlit, Flask, PySpark, Airflow),
                                     Sigma, Tableau, Javascript (React, React Native), HTML, CSS, dbt, Time-series
                                     analysis, Optimization, NLP, RegEx, SQL, Data modeling, Linux, Semi-structured data,
-                                    AWS,
-                                    LaTeX, Microsoft Office Suite
-                                </code>
+                                    AWS, LaTeX, Microsoft Office Suite
                             </li>
                             <li style={{textAlign: "start"}}>
-                                <code>
                                     Snowflake (SnowPro Data Engineer), Dataiku (Designer Core), Alation (Certified
                                     Professional), Fivetran (Partner Certification), dbt (Certified Developer)
-                                </code>
                             </li>
                         </ul>
                     </div>
@@ -106,29 +109,21 @@ function CVPage() {
                     <div className="App-cv-content">
                         <ul style={{marginTop: "-25px"}}>
                             <li style={{textAlign: "start"}}>
-                                <code>
                                     Consulted and helped deliver Snowflake’s data cloud solution for the 2nd largest foodservice
-                                    distributor in the United States, <code style={{fontWeight: "900"}}>USFoods</code>, implementing a modernized data stack (Snowflake,
-                                    Fivetran, dbt, great_expectations, Sigma, Alation) following Snowflake’s best practices.
-                                </code>
+                                    distributor in the United States, <b>USFoods</b>, implementing a modernized data stack (<b>Snowflake,
+                                    Fivetran, dbt, great_expectations, Sigma, Alation</b>) following Snowflake’s best practices.
                             </li>
                             <li style={{textAlign: "start"}}>
-                                <code>
                                     Architected Source-To-Target mappings and implemented data transformations, building a more
-                                    structured and robust dimensional model with CI/CD engineering best practices in dbt Cloud.
-                                </code>
+                                structured and robust <b>dimensional model</b> with CI/CD engineering best practices in <b>dbt Cloud</b>.
                             </li>
                             <li style={{textAlign: "start"}}>
-                                <code>
                                     Presented to critical stakeholders and project-level managers, communicating closely with the
                                     client for agile development and change requests, and building strong client relationships.
-                                </code>
                             </li>
                             <li style={{textAlign: "start"}}>
-                                <code>
                                     Optimized and sped up minute load times of the BI team’s Tableau dashboards into seconds by
                                     transforming legacy data sources and queries into views with the newer modernized architecture.
-                                </code>
                             </li>
                         </ul>
                     </div>
