0bf6edf45a4c7777f4b6d630d0eae01d89b7d5b8
David <ddaviddn109@gmail.com>
2023-05-14 19:02:48 -0400

    Adding additional changes to personal page.

--- src/components/PersonalPage/PersonalPage.js
+++ src/components/PersonalPage/PersonalPage.js
@@ -8,8 +8,6 @@ import indianaBeach from "../../assets/PersonalImages/indiana_beach.jpg";
 import indianaBeach2 from "../../assets/PersonalImages/beach.jpg";
 import indianaCamp from "../../assets/PersonalImages/indiania_camp.jpg";
 import chicagoSushi from "../../assets/PersonalImages/chicago_sushi.jpg";
-import interior1 from "../../assets/PersonalImages/interior1.jpg";
-import interior2 from "../../assets/PersonalImages/interior2.jpg";
 import korCouple from "../../assets/PersonalImages/kor_couple.jpg";
 import orlando from "../../assets/PersonalImages/orlando.jpg";
 import crHotel from "../../assets/PersonalImages/cr_hotel.jpg";
@@ -27,46 +25,50 @@ function HomePage() {
                     <p className="App-personal-text-small">
                         <code>
                             former limewire and aol instant messenger user.
+                            <code style={{marginTop: '35px', fontSize: '25px'}}>
+                                former limewire and aol instant messenger user... just kidding :)<code/>
+                                <p/>
+                                data professional by day,
+                                graduate student at
+                                georgia <p/> institute of technology
+                                by night.
+                            </code>
                             <p/>
-                            data professional by day,
-                            graduate student at
-                            georgia <p/> institute of technology
-                            by night.
-                        </code>
-                        <p/>
-                        <hr style={{width: '100%'}}/>
-                        {/*<p />*/}
-                        <div/>
-                        {/*<hr className="rounded">*/}
+                            <hr style={{width: '100%'}}/>
+                            {/*<p />*/}
+                            <div/>
+                            {/*<hr className="rounded">*/}
 
-                        <code style={{marginTop: '35px', fontSize: '25px'}}>
-                            outside of learning, i enjoy: <p/>
-                            <code/>
-                            <code style={{fontSize: '18px', marginLeft: '30px'}}>
-                                listening to music and playing guitar. <p/>
+                            <code style={{marginTop: '35px', fontSize: '25px'}}>
+                                outside of learning, i enjoy: <p/>
+                                <code/>
+                                <code style={{fontSize: '18px', marginLeft: '30px'}}>
+                                    listening to music and playing guitar. <p/>
+                                </code>
+                                <code style={{fontSize: '18px', marginLeft: '30px'}}>
+                                    i like indie, alternative, rock (70's +), classical, jpn. city pop, opm. <p/>
+                                </code>
+                                <code style={{fontSize: '18px', marginLeft: '30px'}}>
+                                    traveling and taking vibrant and bright photos of the world. <p/>
+                                </code>
+                                <code style={{fontSize: '18px', marginLeft: '30px'}}>
+                                    i enjoy photos that are nostalgic and emotional, especially with contrasting
+                                    colors. <p/>
+                                </code>
+                                <code style={{fontSize: '18px', marginLeft: '30px'}}>
+                                    watching films that are emotionally impactful and inspire thought-provoking
+                                    conversation. <p/>
+                                </code>
+                                <code style={{fontSize: '18px', marginLeft: '30px'}}>
+                                    i appreciate dystopian, psychological thrillers, romance, and crime movies. <p/>
+                                </code>
+                                <code style={{fontSize: '18px', marginLeft: '30px'}}>
+                                    researching on up-to-date financial trends and finding ways to save money. <p/>
+                                </code>
+                                <code style={{fontSize: '18px', marginLeft: '30px'}}>
+                                    i am a huge credit card points nerd and i am always looking for great deals. <p/>
+                                </code>
                             </code>
-                            <code style={{fontSize: '18px', marginLeft: '30px'}}>
-                                i like indie, alternative, rock (70's +), classical, jpn. city pop, opm. <p/>
-                            </code>
-                            <code style={{fontSize: '18px', marginLeft: '30px'}}>
-                                traveling and taking vibrant and bright photos of the world. <p/>
-                            </code>
-                            <code style={{fontSize: '18px', marginLeft: '30px'}}>
-                                i enjoy photos that are nostalgic and emotional, especially with contrasting colors. <p/>
-                            </code>
-                            <code style={{fontSize: '18px', marginLeft: '30px'}}>
-                                watching films that are emotionally impactful and inspire thought-provoking conversation. <p/>
-                            </code>
-                            <code style={{fontSize: '18px', marginLeft: '30px'}}>
-                                i appreciate dystopian, psychological thrillers, romance, and crime movies. <p/>
-                            </code>
-                            <code style={{fontSize: '18px', marginLeft: '30px'}}>
-                                researching on up-to-date financial trends and finding ways to save money. <p/>
-                            </code>
-                            <code style={{fontSize: '18px', marginLeft: '30px'}}>
-                                i am a huge credit card points nerd and i am always looking for great deals. <p/>
-                            </code>
-                        </code>
                     </p>
                     <img className="App-personal-img" src={tokyoImage} alt="vibrant_skyline"/>
                 </div>
@@ -87,7 +89,7 @@ function HomePage() {
                 </div>
             </div>
             <div className="section">
-                <div className="App-body-light-full" >
+                <div className="App-body-light-full">
                     <img className="App-personal-follow-img" src={indianaBeach2} alt="rooftop"/>
                 </div>
             </div>
@@ -143,12 +145,14 @@ function HomePage() {
                         <code>end.</code>
                     </p>
                     <p>
-                        <code style={{fontSize: "25px"}}>more images will appear as I continue exploring the world :)</code>
+                        <code style={{fontSize: "25px"}}>more images will appear as I continue exploring the world
+                            :)</code>
                     </p>
                 </div>
             </div>
         </div>
-    </>);
+    </>
+);
 }
 
 export default HomePage;
