54ab4a7d1fabf21e44a39e4a1d193a46054c4df9
David <ddaviddn109@gmail.com>
2023-06-08 21:58:32 -0400

    Faster react router.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import {createBrowserRouter, RouterProvider,} from "react-router-dom";
-import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+// import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import ErrorPage from "./components/ErrorPage";
@@ -15,34 +16,44 @@ import ContactPage from "./components/ContactPage/ContactPage";
 // const path_var = '/portfolio'
 const path_var = ''
 
-const router = createBrowserRouter([
-// const router = ([
-    {
-        path: path_var + "/",
-        element: <App/>,
-        errorElement: <ErrorPage/>,
-    },
-    {
-        path: path_var + "/personal",
-        element: <PersonalPage/>
-    },
-    {
-        path: path_var + "/experience",
-        element: <CVPage/>
-    },
-    {
-        path: path_var + "/projects",
-        element: <ProjectPage/>
-    },
-    {
-        path: path_var + "/contact",
-        element: <ContactPage/>
-    },
-])
+// const router = createBrowserRouter([
+// // const router = ([
+//     {
+//         path: path_var + "/",
+//         element: <App/>,
+//         errorElement: <ErrorPage/>,
+//     },
+//     {
+//         path: path_var + "/personal",
+//         element: <PersonalPage/>
+//     },
+//     {
+//         path: path_var + "/experience",
+//         element: <CVPage/>
+//     },
+//     {
+//         path: path_var + "/projects",
+//         element: <ProjectPage/>
+//     },
+//     {
+//         path: path_var + "/contact",
+//         element: <ContactPage/>
+//     },
+// ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        {/*<RouterProvider router={router}/>*/}
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />}/>
+                <Route path="/personal" element={<PersonalPage />}/>
+                <Route path="/experience" element={<CVPage />}/>
+                <Route path="/projects" element={<ProjectPage />}/>
+                <Route path="/contact" element={<ContactPage />}/>
+                <Route path="*" element={<ErrorPage />}/>
+            </Routes>
+        </Router>
     </React.StrictMode>
 );
